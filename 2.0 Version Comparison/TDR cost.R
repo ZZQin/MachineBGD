33b3603a8dc583aff9ad6d380a9b5a66cb098863
Zhizhen Qin <zhizh@LTGVA160.net.stoptb.org>
2021-10-21 16:06:35 +0200

    TDR cost

--- 2.0 Version Comparison/TDR cost.R
+++ 2.0 Version Comparison/TDR cost.R
@@ -26,24 +26,81 @@ D7_Cost <- read_csv("2.0 Version Comparison/Results/D7_Cost.csv")
 Q2_Cost <- read_csv("2.0 Version Comparison/Results/Q2_Cost.csv")
 Q3_Cost <- read_csv("2.0 Version Comparison/Results/Q3_Cost.csv")
 
-names(D6_Cost)
-
 D6_Cost$AI <- "CAD4TBv6"
 D7_Cost$AI <- "CAD4TBv7"
 Q2_Cost$AI <- "qXRv2"
 Q3_Cost$AI <- "qXRv3"
 
 Cost <- rbind(D6_Cost, D7_Cost, Q2_Cost, Q3_Cost)
+colnames(Cost) <- gsub(" ","",colnames(Cost))
 rm(D6_Cost, D7_Cost, Q2_Cost, Q3_Cost)
-Cost <- Cost[, -c(2:4, 9:13)]
-Cost$XpertSaved. <- (1-Cost$`CAD pos`)
-names(Cost) 
-names(Cost) <- c("Score", "Sens", "Spec", "CAD_pos", "PPV", )
+
+Cost <- Cost[, -c(2:4, 9:15, 18)]
+
+Cost$Sens <- as.numeric(sub("%","",Cost$Sens))/100
+Cost$Spec <- as.numeric(sub("%","",Cost$Spec))/100
+Cost$CADpos <- as.numeric(sub("%","",Cost$CADpos))/100
+Cost$PPV <- as.numeric(sub("%","",Cost$PPV))/100
+Cost$Totalcostforconfirmatorytest <- as.numeric(sub(",", "", sub("\\$","",Cost$Totalcostforconfirmatorytest)))
+Cost$Costpercase <- as.numeric(sub("\\$","",Cost$Costpercase))
+
+Cost$One <- 1
+Cost$XpertSaved. <- (1-Cost$CADpos)
+names(Cost)[1] <- "Score" 
+
+
+Cost$XpertTest <- Cost$Totalcostforconfirmatorytest/XpertCost
+Cost$Xpertsaved <- 1-Cost$XpertTest/54125
+
+
+
+
+# qXR ----
+Cost0 <- Cost
+Cost <- Cost[Cost$AI %in% c("qXRv2", "qXRv3"), ]
+Cost$AI <- as.character(Cost$AI)
+
+
+### d. Sensitivity vs Score --------------------
+base <- ggplot(Cost, aes(Score, Sens)) + geom_path(aes(color = AI)) + scale_fill_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) +  scale_color_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) 
+# + geom_ribbon(aes(x = Score, ymin = Sens_L, ymax = Sens_H, color = AI, fill = AI), alpha= 0.2) 
+
+sensCurveqXR <- base + theme_light() + labs(x = "Abnormality Score", y= "Sensitivity", subtitle = paste0("qXRv2 vs qXRv3: The Sensitivity vs abnormality score (n=", length(MDF$PID_OMRS), ")")) +theme(legend.position = c(5,5),legend.justification = c("left", "bottom")) + theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + scale_x_continuous(minor_breaks = seq(0 , 100, 10), breaks = seq(0, 100, 10))+ scale_y_continuous(minor_breaks = seq(0 , 1, 0.1), breaks = seq(0, 1, 0.1)) + geom_vline(xintercept = c(50, 60, 70), linetype="dotted", color = "black", size=1)  
+
+
 
 ### e. Xpert Saved vs Score --------------------
-base <- ggplot(Cost, aes(Score, XpertSaved.)) + geom_path(aes(color = DeepLearningSystem))  + scale_fill_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) +  scale_color_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) 
+base <- ggplot(Cost, aes(Score, XpertSaved.)) + geom_path(aes(color = AI))  + scale_fill_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) +  scale_color_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) 
 
-XpertSavingCurveqXR <- base + theme_light()  + labs(x = "e. Abnormality Score", y= "Xpert Saved", subtitle = paste0("qXRv2 vs qXRv3: The Xpert Saved vs abnormality score (n=", length(MDF$PID_OMRS), ")")) + theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + scale_x_continuous(minor_breaks = seq(0 , 100, 10), breaks = seq(0, 100, 10))+ scale_y_continuous(minor_breaks = seq(0 , 1, 0.1), breaks = seq(0, 1, 0.1))+ geom_vline(xintercept = c(80, 70), linetype="dotted", color = "black", size=1) +theme(  legend.position = c(0.05,0.95),  legend.justification = c("left", "top"))
+XpertSavingCurveqXR <- base + theme_light()  + labs(x = "e. Abnormality Score", y= "Xpert Saved", subtitle = paste0("qXRv2 vs qXRv3: The Xpert Saved vs abnormality score (n=", length(MDF$PID_OMRS), ")")) + theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + scale_x_continuous(minor_breaks = seq(0 , 100, 10), breaks = seq(0, 100, 10))+ scale_y_continuous(minor_breaks = seq(0 , 1, 0.1), breaks = seq(0, 1, 0.1))+ geom_vline(xintercept = c(50, 60, 70), linetype="dotted", color = "black", size=1) +theme(  legend.position = c(0.05,0.95),  legend.justification = c("left", "top"))
 
 
 
+# CAD4TB ----
+Cost <- Cost0
+Cost <- Cost[Cost$AI %in% c("CAD4TBv6", "CAD4TBv7"), ]
+Cost$AI <- as.character(Cost$AI)
+
+
+
+### d. Sensitivity vs Score --------------------
+base <- ggplot(Cost, aes(Score, Sens)) + geom_path(aes(color = AI)) + scale_fill_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) +  scale_color_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) 
+# + geom_ribbon(aes(x = Score, ymin = Sens_L, ymax = Sens_H, color = AI, fill = AI), alpha= 0.2) 
+
+sensCurveCAD4TB <- base + theme_light()  + labs(x = "Abnormality Score", y= "Sensitivity", subtitle = paste0("CAD4TBv6 vs CAD4TBv7: The Sensitivity vs abnormality score (n=", length(MDF$PID_OMRS), ")")) +theme(legend.position = c(5,5),legend.justification = c("left", "bottom")) + theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + scale_x_continuous(minor_breaks = seq(0 , 100, 10), breaks = seq(0, 100, 10))+ scale_y_continuous(minor_breaks = seq(0 , 1, 0.1), breaks = seq(0, 1, 0.1)) + geom_vline(xintercept = c(50, 60, 70), linetype="dotted", color = "black", size=1)  
+
+
+
+### e. Xpert Saved vs Score --------------------
+base <- ggplot(Cost, aes(Score, XpertSaved.)) + geom_path(aes(color = AI))  + scale_fill_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) +  scale_color_manual(values=c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")) 
+
+XpertSavingCurveCAD4TB <- base + theme_light()  + labs(x = "e. Abnormality Score", y= "Xpert Saved", subtitle = paste0("CAD4TBv6 vs CAD4TBv7: The Xpert Saved vs abnormality score (n=", length(MDF$PID_OMRS), ")")) + theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + scale_x_continuous(minor_breaks = seq(0 , 100, 10), breaks = seq(0, 100, 10))+ scale_y_continuous(minor_breaks = seq(0 , 1, 0.1), breaks = seq(0, 1, 0.1))+ geom_vline(xintercept = c(50, 60, 70), linetype="dotted", color = "black", size=1) +theme(legend.position = c(0.05,0.95),  legend.justification = c("left", "top"))
+
+
+
+### Save --------------------
+
+tiff("2.0 Version Comparison/2 Curves.tif", width = 10, height = 10, units = "in", res = 100)
+require(gridExtra)
+grid.arrange(sensCurveqXR, XpertSavingCurveqXR, sensCurveCAD4TB, XpertSavingCurveCAD4TB,  nrow=2)
+dev.off()
